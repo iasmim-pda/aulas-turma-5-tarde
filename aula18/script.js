3c72ec5e595ac1b0550e6b0ef8058b89ff095b2e
Iasmim Santos <iasmin.santos-ext@ab-inbev.com>
2024-01-31 21:13:44 -0300

    Aula 18

--- aula18/script.js
+++ aula18/script.js
@@ -9,3 +9,53 @@ document.write(`<h2>Olá, sou o ${variavel}!</h2>`);
 // document.createElement
 // document.getElementById
 // document.getElementByClass
+let dezoito = "18";
+let palavra = "Phelipe".replace("e", "i"); // retornar Philipi
+
+"A idade do Phelipe é: 19".replace("19", "20"); //"A idade do Phelipe é: 20
+
+"A natália é uma densenvolvedora incrivel".replace("incrivel", "maravilhosa")
+//Como substituir mais de uma letra? //Comece pesquisando sobre REGEX
+
+
+
+palavra.length;
+palavra.toLowerCase(); //retornar em minusculo
+palavra.toUpperCase(); //retornar em maiusculo
+
+
+
+
+let alunoNota10 = "Phelipe Cezario" //
+
+//Posição 0 da minha String -> P
+//Posição 1 da minha String -> h
+//Posição 2 da minha String -> e
+//Posição 3 da minha String -> l
+//Posição 4 da minha String -> i
+//Posição 5 da minha String -> p
+//Posição 6 da minha String -> e
+
+//Caso eu quisesse apenas o primeiro nome dessa string?
+//Para selecionar apenas uma parte da string, usamos o metodo slice
+alunoNota10.slice(0, 6) // Phelipe
+
+//Há varias formas de passar argumento para os métodos. Leia a documentação
+//do MDN para entender os casos de uso
+
+
+
+
+//nomes de produtos, preços
+
+//Quando backend nao facilita a vida do front:
+//DA RUIM
+// Tem um tipo de dado
+//COR: a gente pode mandar "amarelo" 
+
+//"amarelo" e "AMARELO"
+
+// if(cor.toLowerCase() == "amarelo")
+
+// if(cor == "amarelo")
+// if(cor == "AMARELO")
