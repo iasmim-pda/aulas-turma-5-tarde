164e406300eeb1f0e90fc67eb9506726800599f2
Iasmim Santos <iasmin.santos-ext@ab-inbev.com>
2024-01-31 21:12:28 -0300

    Aula 18

---
+++ aula18/script.js
@@ -0,0 +1,11 @@
+document.write("<h1>Hello world!</h1>");
+
+let variavel = prompt("Escreva seu nome:");
+
+
+//Estudem template strings
+document.write(`<h2>Olá, sou o ${variavel}!</h2>`);
+
+// document.createElement
+// document.getElementById
+// document.getElementByClass
